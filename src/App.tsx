a85df711e9b17b8a73dd9339ec80208267ab0f31
Francesco Battaglini <francesco.battaglini@lexicondigital.com.au>
2021-12-21 10:40:10 +1100

    lading page

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LandingPage } from "./LandingPage";
 import { Header } from "./Header";
+import { LoginPage } from "./LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
+      <div className="App-root-element">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  );
-}
-
-function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
---
+++ src/LandingPage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button, Typography, Image } from "antd";
+import { Link } from "react-router-dom";
+
+export function LandingPage() {
+  return (
+    <>
+      <Typography className="LandingPage-header">Food fighters</Typography>
+      <Image
+        className="LandingPage-graphics"
+        src={`${process.env.PUBLIC_URL}/landing-page-graphics.png`}
+      />
+      <Link to="login">
+        <Button type="primary" block size="large">
+          Get started
+        </Button>
+      </Link>
+    </>
+  );
+}
