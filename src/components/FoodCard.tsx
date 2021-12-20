e72631eceadbe23455d77cef0b673d4b93eac683
Jordan <jordan@Jordans-MacBook-Pro.local>
2021-12-21 10:02:29 +1100

    Added FoodCard component

---
+++ src/components/FoodCard.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card } from "antd";
+
+const FoodCard = () => {
+  return (
+    <>
+      <Card
+        title="Default size card"
+        extra={<a href="#">More</a>}
+        style={{ width: 300 }}
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+      <Card
+        size="small"
+        title="Small size card"
+        extra={<a href="#">More</a>}
+        style={{ width: 300 }}
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+    </>
+  );
+};
+
+export default FoodCard;
