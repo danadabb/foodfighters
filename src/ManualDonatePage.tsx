9266abfe437ade555646890962aeef8451a60779
Dana <dana.dabbagh@ladbrokes.com.au>
2021-12-21 13:16:37 +1100

    WIP: manual donate

---
+++ src/ManualDonatePage.tsx
@@ -0,0 +1,42 @@
+import { Button, Col, Input, Row, Form, DatePicker } from "antd";
+
+const ManualDonatePage = () => {
+  return (
+    <>
+      <Row justify="center" style={{ marginTop: "40px" }}>
+        <Col span={20}>
+          <Form layout="vertical">
+            <Form.Item label="Product Name" required>
+              <Input placeholder="Product Name" />
+            </Form.Item>
+            <Form.Item label="Manufacturer" required>
+              <Input placeholder="Manufacturer" />
+            </Form.Item>
+            <Form.Item label="Product Name" required>
+              <DatePicker />
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+      <Row justify="space-around">
+        <Col span={20}>
+          <Button
+            ghost
+            type="primary"
+            style={{
+              width: "48%", // flex was giving me grief, TODO: fix this
+              marginRight: "4%",
+            }}
+          >
+            Back
+          </Button>
+          <Button style={{ width: "48%" }} type="primary">
+            Next
+          </Button>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ManualDonatePage;
