8a62a7b7d8cffd070beff812da8253e88c53b0e5
Jordan <jordan@Jordans-MacBook-Pro.local>
2021-12-21 11:57:20 +1100

    Continued working on FoodCard

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Typography } from "antd";
 
 type foodCardProps = {
     cardTitle: string,
@@ -9,12 +9,33 @@ type foodCardProps = {
 }
 
 const FoodCard = ({cardTitle, expiryDate, brandName, foodImage}: foodCardProps): JSX.Element => {
+  const {Title, Text} = Typography;
+
+  const textBlue = '#002766';
+
+  const foodCardContentStyle = {
+    display: 'flex', 
+    flexDirection: 'row' as 'row', 
+    justifyContent: 'flex-start', 
+    alignItems: 'center', 
+    maxWidth: '100%',
+    padding: '5px',
+  }
+
+  const foodCardImageContainerStyle = {
+    padding: 0, 
+    margin: 0,
+    maxWidth: '50%',
+
+  }
 
   const foodCardInfoStyle = {
     display: "flex",
     flexDirection: "column" as "column",
     justifyContent: "space-between",
     alignItems: "flex-start",
+    marginLeft: "15px",
+    maxWidth: '50%',
   }
 
 const foodCardInfoGroupStyle = {
@@ -28,19 +49,21 @@ const foodCardInfoGroupStyle = {
     <>
       <Card
         size="small"
-        style={{ maxWidth: "400px", margin: "0 auto" }}
+        style={{ maxWidth: "500px", margin: "10px auto", backgroundColor: '#E6F7FF', border: "1px solid black"}}
       >
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '250px'}}>
-            <img src={foodImage} height={150} alt={`${cardTitle}`} />
+        <div style={foodCardContentStyle}>
+            <div style={foodCardImageContainerStyle}>
+              <img src={foodImage} alt={`${cardTitle}`} style={{maxHeight: '150px'}} />
+              </div>
             <div style={foodCardInfoStyle}>
-                  <p style={{fontWeight: "bold", fontSize: "1.2rem"}}>{cardTitle}</p>
+                  <Title level={4} style={{color: textBlue}}>{cardTitle}</Title>
                 <div style={foodCardInfoGroupStyle}>
-                  <p>Expiry Date:</p>
-                  <p style={{fontWeight: "bold"}}>{expiryDate}</p>
+                  <Text type="secondary">Expiry Date:</Text>
+                  <Text strong style={{color: textBlue}}>{expiryDate}</Text>
                 </div>
                 <div style={foodCardInfoGroupStyle}>
-                  <p>Brand:</p>
-                  <p style={{fontWeight: "bold"}}>{brandName}</p>
+                  <Text type="secondary">Brand:</Text>
+                  <Text strong style={{color: textBlue}}>{brandName}</Text>
                 </div>
             </div>
         </div>
