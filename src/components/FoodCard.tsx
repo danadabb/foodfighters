1c22cfdb533f2e98c88bc27144e1ab3e9c7d8a7b
Jordan <jordan@Jordans-MacBook-Pro.local>
2021-12-21 12:14:47 +1100

    Continued working on FoodCard

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -2,32 +2,38 @@ import React from "react";
 import { Card, Typography } from "antd";
 
 type foodCardProps = {
-    cardTitle: string,
-    expiryDate: string,
-    brandName: string,
-    foodImage: string,
-}
+  cardTitle: string;
+  expiryDate: string;
+  brandName: string;
+  foodImage: string;
+};
 
-const FoodCard = ({cardTitle, expiryDate, brandName, foodImage}: foodCardProps): JSX.Element => {
-  const {Title, Text} = Typography;
+const FoodCard = ({
+  cardTitle,
+  expiryDate,
+  brandName,
+  foodImage,
+}: foodCardProps): JSX.Element => {
+  const { Title, Text } = Typography;
 
-  const textBlue = '#002766';
+  const textBlue = "#002766";
 
   const foodCardContentStyle = {
-    display: 'flex', 
-    flexDirection: 'row' as 'row', 
-    justifyContent: 'flex-start', 
-    alignItems: 'center', 
-    maxWidth: '100%',
-    padding: '5px',
-  }
+    display: "flex",
+    flexDirection: "row" as "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    maxWidth: "100%",
+    padding: "5px",
+  };
 
   const foodCardImageContainerStyle = {
-    padding: 0, 
+    padding: 0,
     margin: 0,
-    maxWidth: '50%',
-
-  }
+    width: '100px',
+    height: '120px',
+    backgroundColor: 'white'
+  };
 
   const foodCardInfoStyle = {
     display: "flex",
@@ -35,37 +41,52 @@ const FoodCard = ({cardTitle, expiryDate, brandName, foodImage}: foodCardProps):
     justifyContent: "space-between",
     alignItems: "flex-start",
     marginLeft: "15px",
-    maxWidth: '50%',
-  }
+    maxWidth: "50%",
+  };
 
-const foodCardInfoGroupStyle = {
+  const foodCardInfoGroupStyle = {
     display: "flex",
     flexDirection: "column" as "column",
     justifyContent: "center",
     alignItems: "flex-start",
-}
+  };
 
   return (
     <>
       <Card
         size="small"
-        style={{ maxWidth: "500px", margin: "10px auto", backgroundColor: '#E6F7FF', border: "1px solid black"}}
+        style={{
+          maxWidth: "500px",
+          margin: "10px auto",
+          backgroundColor: "#E6F7FF",
+          border: "1px solid black",
+        }}
       >
-        <div style={foodCardContentStyle}>
-            <div style={foodCardImageContainerStyle}>
-              <img src={foodImage} alt={`${cardTitle}`} style={{maxHeight: '150px'}} />
-              </div>
-            <div style={foodCardInfoStyle}>
-                  <Title level={4} style={{color: textBlue}}>{cardTitle}</Title>
-                <div style={foodCardInfoGroupStyle}>
-                  <Text type="secondary">Expiry Date:</Text>
-                  <Text strong style={{color: textBlue}}>{expiryDate}</Text>
-                </div>
-                <div style={foodCardInfoGroupStyle}>
-                  <Text type="secondary">Brand:</Text>
-                  <Text strong style={{color: textBlue}}>{brandName}</Text>
-                </div>
+        <div style={foodCardContentStyle} onClick={() => alert("clicked!")}>
+          <div style={foodCardImageContainerStyle}>
+            <img
+              src={foodImage}
+              alt={`${cardTitle}`}
+              style={{height: '100%', width: '100%', objectFit: 'contain' }}
+            />
+          </div>
+          <div style={foodCardInfoStyle}>
+            <Title level={3} style={{ color: textBlue }}>
+              {cardTitle}
+            </Title>
+            <div style={foodCardInfoGroupStyle}>
+              <Text type="secondary">Expiry Date</Text>
+              <Text strong style={{ color: textBlue }}>
+                {expiryDate}
+              </Text>
             </div>
+            <div style={foodCardInfoGroupStyle}>
+              <Text type="secondary">Brand</Text>
+              <Text strong style={{ color: textBlue }}>
+                {brandName}
+              </Text>
+            </div>
+          </div>
         </div>
       </Card>
     </>
