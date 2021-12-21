e274fc2d4cfe5cb98fe42e1929fd5fcc5c316755
Jordan <jordan@Jordans-MacBook-Pro.local>
2021-12-21 11:03:53 +1100

    Continued working on FoodCard

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -1,27 +1,49 @@
 import React from "react";
 import { Card } from "antd";
 
-const FoodCard = () => {
+type foodCardProps = {
+    cardTitle: string,
+    expiryDate: string,
+    brandName: string,
+    foodImage: string,
+}
+
+const FoodCard = ({cardTitle, expiryDate, brandName, foodImage}: foodCardProps): JSX.Element => {
+
+  const foodCardInfoStyle = {
+    display: "flex",
+    flexDirection: "column" as "column",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+  }
+
+const foodCardInfoGroupStyle = {
+    display: "flex",
+    flexDirection: "column" as "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+}
+
   return (
     <>
       <Card
-        title="Default size card"
-        extra={<a href="#">More</a>}
-        style={{ width: 300 }}
-      >
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
-      <Card
         size="small"
-        title="Small size card"
-        extra={<a href="#">More</a>}
-        style={{ width: 300 }}
+        style={{ maxWidth: "400px", margin: "0 auto" }}
       >
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '250px'}}>
+            <img src={foodImage} height={150} alt={`${cardTitle}`} />
+            <div style={foodCardInfoStyle}>
+                  <p style={{fontWeight: "bold", fontSize: "1.2rem"}}>{cardTitle}</p>
+                <div style={foodCardInfoGroupStyle}>
+                  <p>Expiry Date:</p>
+                  <p style={{fontWeight: "bold"}}>{expiryDate}</p>
+                </div>
+                <div style={foodCardInfoGroupStyle}>
+                  <p>Brand:</p>
+                  <p style={{fontWeight: "bold"}}>{brandName}</p>
+                </div>
+            </div>
+        </div>
       </Card>
     </>
   );
