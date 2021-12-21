1d12304cd1edc49b3cb6446c1d3dc06d63d18b82
Dana <dana.dabbagh@ladbrokes.com.au>
2021-12-21 14:36:45 +1100

    Update donate url

--- src/AccountPage.tsx
+++ src/AccountPage.tsx
@@ -1,27 +1,32 @@
 import React from "react";
-import { Button, Divider} from "antd";
+import { Button, Divider } from "antd";
 import { Link } from "react-router-dom";
 import FoodCard from "./components/FoodCard";
-import { stringify } from "querystring";
-import data from '././utils/mockCardData.json'
-
+import data from "././utils/mockCardData.json";
 
 function AccountPage() {
-   var foodlist = Object.entries(data)
+  var foodlist = Object.entries(data);
 
   return (
     <>
       <h1>Welcome,Jane!</h1>
-      <Link to="/scan">
-        <Button type="primary" block size="large" >
+      <Link to="/donate">
+        <Button type="primary" block size="large">
           Donate
         </Button>
       </Link>
-      <Divider type='vertical'/>
+      <Divider type="vertical" />
       <h3>My Listings</h3>
-      {foodlist.slice(0,2).map(([_, val]) => <FoodCard cardTitle={val.title} expiryDate={val.expiryDate} brandName={val.brandName} foodImage={val.pictureUrl} />)}
+      {foodlist.slice(0, 2).map(([_, val]) => (
+        <FoodCard
+          cardTitle={val.title}
+          expiryDate={val.expiryDate}
+          brandName={val.brandName}
+          foodImage={val.pictureUrl}
+        />
+      ))}
     </>
   );
-};
+}
 
 export default AccountPage;
